b55da4ae2353e0bc8a9de98d83a1adc94e22ab2c
alessio <alessio.cirillo.1995@gmail.com>
2020-10-20 15:48:08 +0200

    numeri da 1 a 100

---
+++ js/main.js
@@ -0,0 +1,34 @@
+
+var numeri = [];
+
+i = 1;
+
+//GENERATORE NUEMRI
+while ( i < 101){
+    numeri += i + ',' ;
+    i++;
+    
+    /*if( numeri % 3 == 0){
+        numeri = 'Fizz';
+    
+    } else if (numeri % 3 == 0 && numeri % 5 == 0){
+        numeri = 'FizzBuzz';
+    
+    } */ 
+    
+}
+console.log(numeri);
+
+
+/*while ( i % 3 == 0 && i % 5 == 0){
+    numeri = FizzBuzz;
+}
+
+console.log(numeri);*/
+
+/*if (numeri % 3 == 0 && numeri % 5 == 0){
+    numeri = 'FizzBuzz';
+}
+
+console.log(numeri);*/
+
